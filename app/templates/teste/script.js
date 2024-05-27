7243c0802ae32cf104f9cdfe2c3af54e859fc66d
Aluno da Infinity School <aluno@3D.local>
2024-05-27 19:29:58 -0300

    version 1.0.1

---
+++ app/templates/teste/script.js
@@ -0,0 +1,56 @@
+
+
+// var numero = 50
+// var contagem = 0
+// var soma = 0
+// while (numero>0){
+//     contagem +=1 
+//     numero-=1
+//     console.log(soma+=contagem)
+// }
+
+// var numero = parseFloat(prompt("Digite o numero"))
+// var resultado = 0
+// var contagem = numero
+
+// if (contagem>=0){
+// while (contagem>0){
+//     contagem-=1
+//     if (numero%contagem==0){
+//         resultado+=1
+//     }else{
+//         continue
+//     }
+// }}else{
+// while (contagem<0){
+//     contagem+=1
+//     console.log(contagem)
+//     if (numero%contagem==0){
+//         resultado+=1
+//     }else{
+//         continue
+//     }
+// }}
+
+// if (resultado == 1){
+//     console.log(`O numero ${numero} é primo`)
+// }else {
+//     console.log(`O numero ${numero} não é primo`)
+// }
+
+// var somaNotas = 0
+// var media = 0
+
+// while (true){
+//     var nota1 = parseFloat(prompt("Digite a nota"))
+//     somaNotas += nota1
+//     media += 1
+//     var resposta = prompt("deseja adicionar mais notas?\nS->sim\nN->não")
+//     if (resposta == "n"){
+//         break
+//     }else{
+//         continue
+//     }
+// }
+
+// console.log(`A média é ${somaNotas/media}`)
